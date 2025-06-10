2f1c9dac9447947672dd9cf4e43cf90c7db98954
Artavazd Barseghyan <artavazd1@gmail.com>
2025-06-10 22:58:10 +0200

    remove dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/common/logger';
-export declare function autoLogExpressRequests(app: any): void;
--- dist/src/common/logger.d.ts
+++
@@ -1,6 +0,0 @@
-import winston from 'winston';
-import 'winston-daily-rotate-file';
-declare const logger: winston.Logger;
-declare const kafkaLogger: winston.Logger;
-declare const accessLogger: winston.Logger;
-export { logger, kafkaLogger, accessLogger };
